3f2941cb66d6c757c4dad4f2688a26e4f4b28633
PooyaY <59162706+pooyay@users.noreply.github.com>
2023-08-29 03:22:37 +0330

    feat: wifi broadcast and post request

--- app/src/main/java/edu/sharif/hwlab/WifiActivity.kt
+++ app/src/main/java/edu/sharif/hwlab/WifiActivity.kt
@@ -1,21 +1,37 @@
 package edu.sharif.hwlab
 
-import android.content.Context
-import android.net.wifi.WifiConfiguration
-import android.net.wifi.WifiManager
-import android.os.Bundle
 //import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
 //import androidx.navigation.findNavController
 //import androidx.navigation.ui.AppBarConfiguration
 //import androidx.navigation.ui.navigateUp
 //import androidx.navigation.ui.setupActionBarWithNavController
+import java.nio.ByteBuffer
+
+import android.content.Context
+import android.net.DhcpInfo
+import android.net.wifi.WifiConfiguration
+import android.net.wifi.WifiInfo
+import android.net.wifi.WifiManager
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import edu.sharif.hwlab.databinding.ActivityWifiBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import java.io.BufferedReader
 import java.io.DataOutputStream
 import java.io.InputStreamReader
+import java.net.DatagramPacket
+import java.net.DatagramSocket
 import java.net.HttpURLConnection
+import java.net.InetAddress
+import java.net.NetworkInterface
+import java.net.SocketException
 import java.net.URL
+import java.net.UnknownHostException
+import kotlin.experimental.and
+
 
 class WifiActivity : AppCompatActivity() {
 
@@ -40,7 +56,7 @@ class WifiActivity : AppCompatActivity() {
         }
     }
 
-    fun setupHotspot(){
+    fun setupHotspot() {
         val esp_ssid = "sakht"
         val esp_password = "12345678"
         val wifiConfig = WifiConfiguration()
@@ -53,6 +69,11 @@ class WifiActivity : AppCompatActivity() {
         wifiManager.reconnect()
     }
 
+
+    fun inetAddressToInt(inetAddress: InetAddress): Int {
+        val ipBytes = inetAddress.address
+        return ByteBuffer.wrap(ipBytes).int
+    }
 
     //    private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityWifiBinding
@@ -70,10 +91,108 @@ class WifiActivity : AppCompatActivity() {
 //        setupActionBarWithNavController(navController, appBarConfiguration)
 
         binding.setupButton.setOnClickListener {
-            postRequest()
+//            postRequest()
+
+            val apiUrl = "http://192.168.4.1/wifi"
+            performApiCall(apiUrl)
         }
 
+//        val apiUrl = "http://your.api.endpoint.com"
+//        performApiCall(apiUrl)
     }
+
+    private fun performApiCall(apiUrl: String) {
+        GlobalScope.launch(Dispatchers.IO) {
+            val result = makeApiCall(apiUrl)
+            // Handle the API response on the main thread if needed
+            launch(Dispatchers.Main) {
+                handleApiResponse(result)
+            }
+        }
+    }
+
+    private fun getBroadcastIpAddress(): String {
+        try {
+            val enumNetworkInterfaces = NetworkInterface.getNetworkInterfaces()
+            while (enumNetworkInterfaces.hasMoreElements()) {
+                val networkInterface = enumNetworkInterfaces.nextElement()
+                if (networkInterface.isLoopback)
+                    continue
+                val interfaceAddress = networkInterface.interfaceAddresses
+                return interfaceAddress.find { x -> x.broadcast != null }?.broadcast?.hostAddress
+                    ?: ""
+            }
+        } catch (e: SocketException) {
+            e.printStackTrace()
+        }
+        return ""
+    }
+
+    fun find_ip() {
+        try {
+            val bip = getBroadcastIpAddress()
+            println("Broadcast IP Address: $bip")
+
+            val socket = DatagramSocket()
+            socket.broadcast = true
+            val sendData = "GET_LOCAL_IP".toByteArray()
+            val sendPacket = DatagramPacket(
+                sendData,
+                sendData.size,
+                InetAddress.getByName(bip),
+                12345
+            )
+            socket.send(sendPacket)
+            val receiveData = ByteArray(1024)
+            val receivePacket = DatagramPacket(receiveData, receiveData.size)
+            socket.receive(receivePacket)
+            val receivedMessage =
+                String(receivePacket.data).slice(IntRange(0, receivePacket.length - 1))
+            println("Received IP Address: $receivedMessage")
+            socket.close()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+
+    private suspend fun makeApiCall(apiUrl: String): String {
+        find_ip()
+        return ""
+        /*val url = URL(apiUrl)
+        val connection = url.openConnection() as HttpURLConnection
+        connection.requestMethod = "POST"
+        connection.doOutput = true
+
+        try {
+            val outputStream = connection.outputStream
+            val postData =
+                "ssid=" + binding.ssidText.text + "&password=" + binding.passwordText.text
+            outputStream.write(postData.toByteArray())
+            outputStream.close()
+
+            val inputStream = BufferedReader(InputStreamReader(connection.inputStream))
+            val response = StringBuilder()
+            var line: String?
+            while (inputStream.readLine().also { line = it } != null) {
+                response.append(line)
+            }
+            return response.toString()
+        } catch (e: Throwable) {
+            //val Log = Logger.getLogger(WifiActivity::class.java.name)
+            e.message?.let { Log.e("Api call", it) }
+            throw e
+        } finally {
+            connection.disconnect()
+        }
+        return ""*/
+    }
+
+    private fun handleApiResponse(result: String) {
+        Toast.makeText(applicationContext, result, Toast.LENGTH_LONG).show()
+        // Handle the API response here
+    }
+
 
 //    override fun onSupportNavigateUp(): Boolean {
 //        val navController = findNavController(R.id.nav_host_fragment_content_wifi)
